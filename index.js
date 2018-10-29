8487df59f882176b2060471b6ed32b9e8ec42049
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2018-10-29 11:29:52 +0900

    #2.7 DOM / If else / Function practice part Two

--- index.js
+++ index.js
@@ -1,23 +1,13 @@
 const title = document.querySelector("#title");
 
-const BASE_COLOR = "rgb(52, 73, 94)";
-const OTHER_COLOR = "#7f8c8d";
+const CLICK_CLASS = "clicked";
 
 function handleClick() {
-  const currentColor = title.style.color;
-  if (currentColor === BASE_COLOR) {
-    // If the color is same as the initial one
-    title.style.color = OTHER_COLOR;
-  } else {
-    // If the color has changed,
-    // we make it into the initial one again
-    title.style.color = BASE_COLOR;
-  }
+  title.classList.toggle(CLICK_CLASS);
 }
 
 function init() {
-  title.style.color = BASE_COLOR;
+  title.addEventListener("click", handleClick);
 }
 
-title.addEventListener("click", handleClick);
 init();
