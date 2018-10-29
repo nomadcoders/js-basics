c62436376c1bae77ee3bce14bd28325a8af7cfb3
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2018-10-29 11:15:55 +0900

    #2.6 DOM / If else / Function practice

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 const title = document.querySelector("#title");
 
-function handleClick(event) {
-  title.style.color = "blue";
+const BASE_COLOR = "rgb(52, 73, 94)";
+const OTHER_COLOR = "#7f8c8d";
+
+function handleClick() {
+  const currentColor = title.style.color;
+  if (currentColor === BASE_COLOR) {
+    // If the color is same as the initial one
+    title.style.color = OTHER_COLOR;
+  } else {
+    // If the color has changed,
+    // we make it into the initial one again
+    title.style.color = BASE_COLOR;
+  }
+}
+
+function init() {
+  title.style.color = BASE_COLOR;
 }
 
 title.addEventListener("click", handleClick);
+init();
