cbb4d95ae904bf85c591be703eb3094f2090f895
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2018-10-29 10:59:42 +0900

    #2.4 Events and event handlers

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const title = document.querySelector("#title");
-title.innerHTML = "Hi! From JS";
-title.style.color = "red";
-document.title = "I own you now";
+
+function handleClick(event) {
+  title.style.color = "blue";
+}
+
+title.addEventListener("click", handleClick);
