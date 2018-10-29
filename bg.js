d8ad31037afdf700b7a02360ff8a05c243fbfdcc
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2018-10-29 16:48:19 +0900

    Image BG

---
+++ bg.js
@@ -0,0 +1,22 @@
+const body = document.querySelector("body");
+
+const IMG_NUMBER = 3;
+
+function paintImage(imgNumber) {
+  const image = new Image();
+  image.src = `images/${imgNumber + 1}.jpg`;
+  image.classList.add("bgImage");
+  body.prepend(image);
+}
+
+function genRandom() {
+  const number = Math.floor(Math.random() * IMG_NUMBER);
+  return number;
+}
+
+function init() {
+  const randomNumber = genRandom();
+  paintImage(randomNumber);
+}
+
+init();
